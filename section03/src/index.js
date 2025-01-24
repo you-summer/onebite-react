63782a0fb581762add278509754003b59b336d56
조연정 <jojoor201@gmail.com>
2025-01-25 01:26:28 +0900

    day7-1

--- section03/src/index.js
+++ section03/src/index.js
@@ -1,1 +1,9 @@
-console.log("안녕 Node.js");
+const moduleData = require("./math");
+
+console.log(moduleData.add(1,2));
+console.log(moduleData.sub(1,2));
+
+// 객체의 구조분해할당을 이용하면 아래 코드도 가능함
+// const {add, sub} = require("/.math");
+
+console.log(moduleData);
---
+++ section03/src/math.js
@@ -0,0 +1,14 @@
+// math 모듈 // 계산기능을 갖고있는 math모듈이라고 볼수 있음
+
+function add(a,b){
+    return a + b;
+}
+
+function sub(a,b){
+    return a - b;
+}
+
+module.exports = {
+    add,
+    sub,
+};

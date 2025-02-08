434270a1014c58e0259178c8491f7425d2e004d5
조연정 <jojoor201@gmail.com>
2025-02-08 20:05:25 +0900

    day14-1

---
+++ section08/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Header from './components/Header'
+import Editor from './components/Editor'
+import List from './components/List'
+
+function App() {
+
+  return (
+    <div className='App' >
+      <Header />
+      <Editor/>
+      <List/>
+    </div>
+  )
+}
+
+export default App
